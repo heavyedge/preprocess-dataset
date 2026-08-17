dcb4cb0fef4e1c82f56d930016d40b70fd97779f
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-08-17 10:10:44 +0900

    maint: modify ci/cd
    Fix setup

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 
 pip install uv
 
+uv pip install --system 'gdown<6.0.0'
 mkdir -p ./_data/v1/profiles ./_data/v1/ca
 
 (
   uv pip install --system -r requirements.txt -r examples/requirements.txt
-  uv pip install --system 'gdown<6.0.0'
 ) &
 requirements_pid=$!
 
